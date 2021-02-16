5df025239455efc58748b13f60c7b4e9aaef2cf7
ytg <yoontg1223@naver.com>
2021-02-16 22:02:03 +0900

    블럭 하나 쌓기, esc 게임 끝내기

--- tetrismap.py
+++ tetrismap.py
@@ -5,6 +5,7 @@ import keyboard
 
 width = 30 
 height = 10
+speed = 0.75
 draw_board(width , height)  # draw a board
 time.sleep(1)  # wait a second
 star_y = 15
@@ -28,14 +29,19 @@ def keyhandler(e):
     elif(e.name == "z"):
         pass
     elif(e.name == "x"):
-        pass       
-
+        pass
+    
+               
 keyboard.hook(keyhandler)
 
 while(star_x < height - 1):
     star_x += 1
     draw_star(star_x, star_y)
-    time.sleep(1)
+    time.sleep(speed)
     draw_star(star_x , star_y, " ")
+    if(star_x == height - 1):
+        draw_star(height - 1, star_y)
 
+
+keyboard.wait('esc')
 curses.endwin()  # return control back to the console
