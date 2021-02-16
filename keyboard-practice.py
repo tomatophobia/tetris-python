56aac6b942fae0a39dd21eec3fc4f81cf5dde9ee
최영서 <lukas@MacBook-Pro.local>
2021-02-16 20:35:45 +0900

    keyboard practice

---
+++ keyboard-practice.py
@@ -0,0 +1,10 @@
+import keyboard
+
+def print_pressed_keys(e):
+    for code in keyboard._pressed_events:
+        line = ', '.join(str(code))
+        print(line,e.name)
+
+
+keyboard.hook(print_pressed_keys)
+keyboard.wait('esc')
